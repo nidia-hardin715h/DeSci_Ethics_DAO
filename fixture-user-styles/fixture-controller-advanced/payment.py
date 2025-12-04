7bc99c880b3960616161c2662d763460043c5f86
nidia-hardin715h <evangelina-z28357963@rambler.ru>
2025-12-05 05:28:46 +0800

    add payment.py, IDecryptionOracle.ts, SecureFederatedDatabase.ts

---
+++ fixture-user-styles/fixture-controller-advanced/payment.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\fixture-user-styles\fixture-controller-advanced\payment.py
+// Generated at: 2025-10-20T08:31:03.159Z
---
+++ frontend/web/src/bridge.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\bridge.rs
+// Generated at: 2025-10-20T08:31:03.176Z
